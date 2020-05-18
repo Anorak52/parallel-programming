f7631fb826f0da0f81b076498fb5e2a387ab3f19
Anorak52 <siber56@yandex.ru>
2020-05-18 16:19:40 +0300

    Upd

--- 1706-4/brazhnikov_ea/SequentialFoxAlg/Source.cpp
+++ 1706-4/brazhnikov_ea/SequentialFoxAlg/Source.cpp
@@ -1,12 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
 #include <iostream>
-#include <cmath>
-#include <ctime>
 #include <random>
-#include <stdexcept>
-
+#include <chrono> //замер времени
 
 using namespace std;
 
@@ -16,9 +10,8 @@ void PrintMatrix(double* matrix, int N) {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++)
 		{
+			cout.width(3);
 			cout << matrix[i * M + j] << " ";
-			for (int k = 0; k < (5 - log10((double)(matrix[i * M + j]))); k++)
-				cout << " ";
 		}
 		cout << endl;
 	}
@@ -32,35 +25,65 @@ void MatrixCreate(double* A, double* B, int size) {
 	}
 }
 
-void Multiplication(double* Ablock, double* Bblock, double* Cblock, int _blockSize)
-{
-	double temp;
-	for (int i = 0; i < _blockSize; i++)
-		for (int j = 0; j < _blockSize; j++) {
-			temp = 0;
-			for (int k = 0; k < _blockSize; k++)
-				temp += Ablock[i * _blockSize + k] * Bblock[k * _blockSize + j];
-			Cblock[i * _blockSize + j] += static_cast<int>(temp * 100) / 100;
-		}
-}
-
 void SequentialAlgorithm(double* A, double* B, double* C, int size) {
 	for (int i = 0; i < size * size; ++i)
 		C[i] = 0;
-	Multiplication(A, B, C, size);
+
+	double temp;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			temp = 0;
+			for (int k = 0; k < size; k++)
+				temp += A[i * size + k] * B[k * size + j];
+			C[i * size + j] = temp;
+		}
+	}
+}
+
+void blockMatrixMult(double* A, double* B, double* C, int size)
+{
+	int block_size = sqrt(size);
+
+	for (int i = 0; i < size * size; ++i)
+		C[i] = 0;
+
+	double temp;
+
+	for (int i = 0; i < size; i += block_size) 
+	{ 
+		for (int j = 0; j < size; j += block_size)
+		{
+			for (int k = 0; k < size; k += block_size) 
+			{
+				for (int a = i; a < min(size, i + block_size); a++) 
+				{
+					for (int b = j; b < min(size, j + block_size); b++) 
+					{
+						temp = 0;
+						for (int c = k; c < min(size, k + block_size); c++) 
+						{
+							temp += A[a * size + c] * B[c * size + b];
+						}
+						C[a * size + b] += temp;
+					}
+				}
+			}
+		}
+	}
 }
 
 int main()
 {
 	int size = 10;
-	double tmp;
-	double* A = &tmp;
-	double* B = &tmp;
-	double* CSeq = &tmp;
+	double* A;
+	double* B;
+	double* CSeq;
+	double* CBlock;
 
 	A = new double[size * size];
 	B = new double[size * size];
 	CSeq = new double[size * size];
+	CBlock = new double[size * size];
 
 	MatrixCreate(A, B, size);
 
@@ -74,10 +97,33 @@ int main()
 		cout << "---------------------" << endl;
 		cout << endl;
 	}
-		
+
+	auto begin = chrono::steady_clock::now();
 	SequentialAlgorithm(A, B, CSeq, size);
-	PrintMatrix(CSeq, size);
+	auto end = std::chrono::steady_clock::now();
 
+	auto elapsed_ms = chrono::duration_cast<chrono::milliseconds>(end - begin);
+
+	cout << endl;
+	cout << "SeqAlgTime: " << elapsed_ms.count() << endl;
+
+	auto beginFox = chrono::steady_clock::now();
+	blockMatrixMult(A, B, CBlock, size);
+	auto endFox = std::chrono::steady_clock::now();
+	auto elapsed_msFox = chrono::duration_cast<chrono::milliseconds>(endFox - beginFox);
+
+	cout << endl;
+	cout << "Block multiplication time:" << elapsed_msFox.count() << endl;
+
+	if (size < 30) {
+		cout << endl;
+		cout << "Seq multiplication:" << endl;
+		PrintMatrix(CSeq, size); 
+		cout << endl;
+		cout << "Block matrix multiplication:" << endl;
+		PrintMatrix(CBlock, size);
+	}
+
+	delete A, B, CSeq, CBlock;
+	
 }
-
-
